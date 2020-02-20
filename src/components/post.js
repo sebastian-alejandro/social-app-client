1b10996357a159a4955ca177bbbe41c3d266eba3
sebastian-alejandro <sebastian.alejandro.hv@gmail.com>
2020-02-20 12:14:29 -0300

    post card details done

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import withStyles from "@material-ui/core/styles/withStyles";
 
+import Link from 'react-router-dom/Link';
+
 import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,7 +10,15 @@ import CardMedia from '@material-ui/core/CardMedia';
 
 const styles = {
     card: {
-        display: 'flex'
+        display: 'flex',
+        marginBottom: 20,
+    },
+    image: {
+        minWidth: 200,
+    },
+    content: {
+        padding: 25,
+        objectFit: 'cover'
     }
 }
 
@@ -16,12 +26,13 @@ class Post extends Component {
     render() {
         const {classes, post: {body, createdAt, userImage, userName, postId, likeCount, commentCount}} = this.props;
         return (
-            <Card>
+            <Card className={classes.card}>
                 <CardMedia
+                    className={classes.image}
                     image={userImage}
                     title="Profile image"/>
-                <CardContent>
-                    <Typography variant="h5">{userName}</Typography>
+                <CardContent className={classes.content}>
+                    <Typography variant="h5" component={Link} to={`/users/${userName}`} color="primary">{userName}</Typography>
                     <Typography variant="body2">{createdAt}</Typography>
                     <Typography variant="body1">{body}</Typography>
                 </CardContent>
